5b83daf8752a4fdd9321225610e9088ab2162234
gojoew-ops <gojoew@gmail.com>
2025-08-21 15:30:21 -0700

    Add files via upload

---
+++ trader-dashboard/pages/api/yahoo.ts
@@ -0,0 +1,11 @@
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.setHeader("Surrogate-Control", "no-store");
+
+  res.status(200).json({ message: "Yahoo API live response" });
+}
