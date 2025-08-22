7ee779eeddc4f3ea556469abe840aa0b34f8fc2a
gojoew-ops <gojoew@gmail.com>
2025-08-22 11:21:48 -0700

    Delete trader-dashboard directory

--- trader-dashboard/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import ApiKeyOverlay from '../components/ApiKeyOverlay';
-import '../styles/globals.css'
-
-export default function App({Component,pageProps}){return (<>
-  <Component {...pageProps} />
-  <ApiKeyOverlay />
-</>)}
